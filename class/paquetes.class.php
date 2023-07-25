1f797f14c3974610a43dcd0aa619991385dcd40e
pabloayala1 <zonalibrecun@gmail.com>
2023-07-24 21:33:27 -0500

    no message

--- class/paquetes.class.php
+++ class/paquetes.class.php
@@ -617,6 +617,38 @@ class Paquetes
         }          
     } 
 
+    public function homeHotels(){
+        $url = $_SERVER['HTTP_HOST'];
+        
+        $path = 'https://app.bookingtrap.com/api/getHomeHotels';
+        $request = new HTTP_Request2();
+
+        $request->setUrl($path);
+        $request->setMethod(HTTP_Request2::METHOD_GET);
+
+        $request->setConfig(array(
+            'follow_redirects' => TRUE
+        ));
+        $request->setHeader(array(
+            'Authorization' => $this->token,
+            'Cookie' => $this->cookie
+        ));   
+        
+        try {
+            $response = $request->send();
+            if ($response->getStatus() == 200) {
+                $respuesta =  (array) json_decode($response->getBody(), true);  
+                return $respuesta;                 
+            }else {
+                echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+                $response->getReasonPhrase();
+            }
+        }
+        catch(HTTP_Request2_Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }          
+    }    
+
     public function getHotelsByRegion($data){
         $url = $_SERVER['HTTP_HOST'];
         

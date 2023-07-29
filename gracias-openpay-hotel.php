3fa68a06f1f7f24e3a5640d9f003b4563383f4dc
pabloayala1 <zonalibrecun@gmail.com>
2023-07-29 01:17:29 -0500

    Se completa proceso de reserva en linea

--- gracias-openpay-hotel.php
+++ gracias-openpay-hotel.php
@@ -22,8 +22,80 @@
     $formRate["montopagado"]   = $respuesta["monto"];
 
     if($estatus === 'completed'){
-        echo "Si se completo";
         $reservafinal = $hotels->requestReservation($formRate);
     }
        
 ?>
+<head>
+    <base href="<?php echo $fn->baseMeta(); ?>">
+    <title>Reservación en línea</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <?php include("templates/head.php"); ?>
+</head>
+
+<body>
+
+    <?php include("templates/preloader.php") ?>
+	<!-- End Preload -->
+
+	<!-- Header================================================== -->
+    <div id="header_1"><?php include("templates/header.php"); ?></div>
+	<!-- End Header 1-->
+
+	<!-- SubHeader =============================================== -->
+	<section class="parallax_window_in" data-parallax="scroll" data-image-src="img/pagoenlinea.jpg" data-natural-width="1400" data-natural-height="470">
+		<div id="sub_content_in">
+			<div id="animate_intro">
+				<h1>
+                    <?php echo $estatus === 'completed' ? 'SU PAGO SE HA REALIZADO CON ÉXITO' : 'SU PAGO NO PUDO SER PROCESADO'; ?>
+                </h1>
+				<p><?php echo $estatus === 'completed' ? 'Felicidades! Prepárate para una experiencia inolvidable' : 'Comunícate con nostros para encontrar una solución'; ?></p>
+			</div>
+		</div>
+	</section>
+	<!-- End section -->
+	<!-- End SubHeader ============================================ -->
+
+	<section class="wrapper">
+		<div class="divider_border"></div>
+
+		<div class="container">
+            <div class="main_title">
+                <?php if($estatus === 'completed'){ ?>
+                    <h2>"<span>Felicidades!, </span> revisa tu correo"</h2>
+                    <p>Te hemos enviado por correo todos los detalles de tu reservación, revisa tu <strong>SPAM</strong>.</p>
+                <?php }else{ ?>
+                    <h2>"Su pago fue rechazado"</h2>
+                    <p>Motivo: <?php echo $mensaje; ?>.</p>                    
+                <?php } ?>
+			</div>
+		</div>
+		<!-- End container -->
+	</section>
+	<!-- End section -->
+    <!-- End container -->
+
+    <footer><?php include("templates/footer.php"); ?></footer>
+	<!-- End footer -->
+
+	<div id="toTop"></div>
+	<!-- Back to top button -->
+
+	<!-- Search Menu -->
+	<div class="search-overlay-menu">
+		<span class="search-overlay-close"><i class="icon_close"></i></span>
+		<form role="search" id="searchform" method="get">
+			<input value="" name="q" type="search" placeholder="Search..." />
+			<button type="submit"><i class="icon-search-6"></i>
+			</button>
+		</form>
+	</div>
+	<!-- End Search Menu -->
+
+	<!-- COMMON SCRIPTS -->
+	<?php include("templates/js.php") ?>
+
+</body>
+
+</html>

1c0e728198dcbcc0ababa22a40b1c085d3aefa3f
pabloayala1 <zonalibrecun@gmail.com>
2023-08-25 15:28:32 -0500

    no message

--- gracias-openpay-hotel.php
+++ gracias-openpay-hotel.php
@@ -2,12 +2,6 @@
     include("templates/language.php"); 
     include("class/allclass.php"); 
 
-    include_once('vendor/autoload.php');
-
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;    
-
     $valoresHotel                = json_decode($_SESSION["reservaHotelera"]);
     $reservaBD                   = $_SESSION["reservaGuardada"];
 
@@ -27,128 +21,11 @@
     $estatus                   = $respuesta["estatus"];
     $formRate["autorizacion"]  = $respuesta["autorizacion"];
     $formRate["montopagado"]   = $respuesta["monto"];
+    $formRate["montopagado"]   = $respuesta["monto"];
+    $formRate["emailAfiliado"] = $emailAfiliado; //Isac: nuevo cambio
+    $formRate["estatus"]       = $estatus; //Isac: nuevo cambio
 
-    $mail = new PHPMailer(true);
-    if($estatus === 'completed'){
-        $reservafinal = $hotels->requestReservation($formRate);   
-
-        $pdf      = $reservafinal["pdf"];
-        $bodyMail = $reservafinal["bodyMail"];          
-        
-        try {
-            //Server settings
-            $mail->SMTPDebug = false;
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'bookingtech.mx';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'notificaciones@bookingtech.mx';                     //SMTP username
-            $mail->Password   = 'Fe!fg8949';                               //SMTP password
-            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-            $mail->SMTPSecure = "tls";
-            $mail->CharSet = 'UTF-8';
-    
-            //Recipients
-            if($myWebSite["cc_email_reservas_uno"] != ''){
-                $mail->setFrom($myWebSite["cc_email_reservas_uno"], 'Mujer Viaja');
-            }else{
-                $mail->setFrom('notificaciones@bookingtech.mx', 'Mujer Viaja');
-            }            
-    
-            //Correo del cliente
-            $mail->addAddress($emailCliente); 
-    
-            //Enviar copia al correo 2 de reservas?
-            if($myWebSite["cc_email_reservas_dos"] != ''){
-                 $mail->addAddress('pabloayaladeveloper@gmail.com');  
-                // $mail->addAddress($myWebSite["cc_email_reservas_dos"]);  
-            }  
-    
-            //Enviar correo al afiliado?
-            if($emailAfiliado != ''){
-                // $mail->addAddress($emailAfiliado);  
-                $mail->addAddress('pabloayaladev@gmail.com');  
-            }       
-    
-            $mail->AddAttachment($pdf); 
-            $mail->addReplyTo($myWebSite["cc_email_reservas_uno"], "Mujer Viaja");  //Poner nombre de la agencia de viajes 
-            $mail->isHTML(true);   
-            $mail->Subject = 'Su reservación se ha realizado con éxito';
-            $mail->Body    = $bodyMail;
-            $mail->AltBody = 'ALT del cuerpo del correo';    
-            $mail->send();
-            } catch (Exception $e) {
-                echo "Error";
-            }          
-    }else{
-        try {
-            //Server settings
-            $mail->SMTPDebug = false;
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'bookingtech.mx';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'notificaciones@bookingtech.mx';                     //SMTP username
-            $mail->Password   = 'Fe!fg8949';                               //SMTP password
-            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-            $mail->SMTPSecure = "tls";
-            $mail->CharSet = 'UTF-8';
-
-            $bodyMail = '
-            <!DOCTYPE html>
-            <html lang="es">            
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Gracias por escribirnos</title>
-            </head>
-            <body> 
-                <h4>Un cliente ha intentado realizar una reservación pero su pago no fue aceptado</h4>
-                <p>
-                    <b>Nombre: </b>'.$valoresHotel->nombre. ' '.$valoresHotel->apellido.'<br> Revise en su plataforma bookingTech para más detalle.';
-        
-                    if($nombre_afiliado != ''){
-                        $bodyMail .='<b>Afiliado: </b>'.$nombre_afiliado.'<br>';
-                    }		
-        
-            $bodyMail .= '</p>
-                <br><br>
-                <h5>Éxito!</h5>
-            </body>    
-            </html>         
-            ';
-    
-            //Recipients
-            $mail->setFrom('notificaciones@bookingtech.mx', 'Mujer Viaja');            
-    
-            //Correos de la agencia para notificarle el error:
-            if($myWebSite["cc_email_reservas_uno"] != ''){
-                // $mail->addAddress($myWebSite["cc_email_reservas_uno"]);  
-                $mail->addAddress('pabloayaladev@gmail.com');  
-            }  
-    
-            //Enviar copia al correo 2 de reservas?
-            if($myWebSite["cc_email_reservas_dos"] != ''){
-                // $mail->addAddress($myWebSite["cc_email_reservas_dos"]);                  
-            }            
-    
-            //Enviar correo al afiliado?
-            if($emailAfiliado != ''){
-                $mail->addAddress($emailAfiliado);  
-                $mail->addAddress('pabloayaladeveloper@gmail.com');  
-            }       
-    
-            $mail->addReplyTo($myWebSite["cc_email_reservas_uno"], "Mujer Viaja");  //Poner nombre de la agencia de viajes 
-            $mail->isHTML(true);   
-            $mail->Subject = 'Su reservación se ha realizado con éxito';
-            $mail->Body    = $bodyMail;
-            $mail->AltBody = 'ALT del cuerpo del correo';    
-            $mail->send();
-            } catch (Exception $e) {
-                echo "Error";
-            }          
-    }
-
-  
+    $reservafinal = $hotels->requestReservation($formRate);  //Isac: nuevo cambio, antes se usaba if
        
 ?>
 <head>

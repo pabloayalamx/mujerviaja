84bfd20ea02bab8decb2a0cf22e244b2be602186
programadorbt4 <139791112+programadorbt4@users.noreply.github.com>
2023-10-30 11:23:21 -0500

    Update terminos-condiciones.php

--- terminos-condiciones.php
+++ terminos-condiciones.php
@@ -6,7 +6,7 @@
 
 <head>
     <title>Términos y condiciones - Mujer Viaja</title>    
-    <meta name="description" content="Nuestros términos y condiciones están diseñados para garantizar una experiencia segura y equitativa para todas nuestras viajeras. Queremos que tu aventura sea inolvidable. Te invitamos a revisar y aceptar nuestros términos antes de embarcarte en esta emocionante travesía con nosotras." />
+    <meta name="description" content="Nuestros términos y condiciones están diseñados para garantizar una experiencia segura para todas nuestras viajeras. Queremos que tu aventura sea inolvidable. Te invitamos a revisar y aceptar nuestros términos antes de embarcarte en esta emocionante travesía con nosotras." />
 <meta name="keywords" content="mujerviaja, mujer viaja,términos y condiciones, política, seguridad, igualdad, acuerdo, normas, responsabilidades, turismo, viajes para mujeres, protección del usuario, condiciones legales" />
     <?php include("templates/head.php"); ?>
 </head>
